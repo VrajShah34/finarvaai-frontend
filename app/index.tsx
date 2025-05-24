1ab38cf49a71a27968d280604ade46ff04264519
Vraj Shah <vrajsshah34@gmail.com>
2025-05-24 10:28:10 +0530

    created react-native app with nativewind(tailwindCSS) for styling

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center font-bold"    >
+      <Text className="">  app/ffd.tsx to edit this screen.</Text>
+    </View>
+  );
+}
